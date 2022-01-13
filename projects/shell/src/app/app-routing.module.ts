0eedcac1129b50a9d4c144315369b44157c7df4e
Benoit Hediard <ben@agorapulse.com>
2022-01-13 23:05:33 +0100

    Remove dynamic routes for dynamic Microfrontends (overcomplicated for nothing)

--- projects/shell/src/app/app-routing.module.ts
+++ projects/shell/src/app/app-routing.module.ts
@@ -1,26 +1,25 @@
 import {loadRemoteModule} from '@angular-architects/module-federation';
 import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
-import {Microfrontend} from "./microfrontends/microfrontend";
+import {environment} from "../environments/environment";
 
 const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
         pathMatch: 'full',
-    }
+    },
+    {
+        path: 'todo',
+        loadChildren: () =>
+            loadRemoteModule({
+                type: 'module',
+                remoteEntry: environment.mfe1RemoteEntryUrl,
+                exposedModule: './Module'
+            }).then(m => m.TodoModule)
+    },
 ];
-
-export function buildRoutes(options: Microfrontend[]): Routes {
-
-    const lazyRoutes: Routes = options.map(o => ({
-        path: o.routePath,
-        loadChildren: () => loadRemoteModule(o).then(m => m[o.ngModuleName])
-    }));
-
-    return [...routes, ...lazyRoutes];
-}
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -1,27 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-import {LookupService} from './microfrontends/lookup.service';
-import {Microfrontend} from './microfrontends/microfrontend';
-import {buildRoutes} from './app-routing.module';
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
     title = 'shell';
-    microfrontends: Microfrontend[] = [];
 
-    constructor(
-        private router: Router,
-        private lookupService: LookupService) {
-    }
-
-    async ngOnInit(): Promise<void> {
-        this.microfrontends = await this.lookupService.lookup();
-        const routes = buildRoutes(this.microfrontends);
-        this.router.resetConfig(routes);
-    }
 }
